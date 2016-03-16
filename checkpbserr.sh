d8aa0fae18c4a28cd58677d1c12af9bcb68eae1d
Carl A. B. Pearson <blank101@gmail.com>
2016-03-16 09:57:19 -0400

    fix check script

--- checkpbserr.sh
+++ checkpbserr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ls *err*]; then
   tput setaf 1
-  more *err* | grep -i -e (error|PBS|make)
+  more *err* | grep -i -regexp='(error|PBS|make)'
   tput setaf 3
   more *err* | grep -i warning
   tput sgr0
