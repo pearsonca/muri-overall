74a434193ef0d2b9c8177b6f19d1f2e4968edc9c
Carl A. B. Pearson <cap10@ufl.edu>
2016-04-20 08:05:57 -0400

    alternative make file stub

---
+++ altMake.mk
@@ -0,0 +1,36 @@
+
+DATADIR := 'input'
+
+TEMPLATEDIR := $(DATADIR)/simtemplates
+
+PEAKDIMS := $(TEMPLATEDIR)/peaktime
+USEDIMS := $(TEMPLATEDIR)/usage
+VARDIMS := $(TEMPLATEDIR)/variability
+SIZEDIMS := $(TEMPLATEDIR)/sizes
+
+PEAKS := $(wildcard $(PEAKDIMS)/*)
+USES := $(wildcard $(USEDIMS)/*)
+VARS := $(wildcard $(VARDIMS)/*)
+SIZES := $(wildcard $(SIZEDIMS)/*)
+
+SIMPATH := ../scala-commsim
+SIM := $(SIMPATH)/target/start
+
+DIGPATH := ../montreal-digest
+MKUSERS := $(DIGPATH)/mk_users.R
+
+PARSED := input/user.RData input/censor.RData input/userPrefs.RData input/locClusters.RData input/loc_probs.csv
+
+# in this rule, % is intended to be 001, 002, ... - sample ids
+define simsrc
+output/simsdata/$(1)/$(2)/$(3)/$(4)/%.src: $(MKUSERS) $(1) $(2) $(3) $(4) | output/simsdata/$(1)/$(2)/$(3)/$(4)/
+	@echo use $^ to make $@
+	$(RPATH) $^ $* > $@
+	@touch $@
+endef
+
+define simout
+output/simsdata/$(1)/$(2)/$(3)/$(4)/%.csv: $(SIM) $(1) $(2) $(3) $(4) | output/simsdata/$(1)/$(2)/$(3)/$(4)/
+	@echo use $^ to make $@
+	@touch $@
+endef
